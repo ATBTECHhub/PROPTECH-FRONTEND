b0800c3ee5c9e42816f706337e58e0a6efaa4ad8
the-geniustech <holumidey70@gmail.com>
2024-09-01 05:06:32 +0100

    Landing page review and modification

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,17 +1,17 @@
 import { CiSearch } from "react-icons/ci";
 const News = () => {
   return (
-    <div className="text-center items-center flex flex-col pt-[10px] pb-[50px] text-black">
-      <h1 className="text-[1.2rem] md:text-[2rem] leading-12 pt-[30px] md:pt-[73px] text-[#2B3F58] font-bold">
+    <div className="flex flex-col items-center px-4 pt-[10px] pb-[50px] text-black text-center">
+      <h1 className="pt-[30px] md:pt-[73px] font-bold text-[#2B3F58] text-[1.2rem] md:text-[2rem] leading-12">
         Featured Accomodation
       </h1>
       <p className="pt-[5px] pb-[25px] text-[#2B3F58] text-[0.9rem] md:text-[1.5rem]">
         Experience Comfort and Luxury in Everystay
       </p>
-      <p className="text-[#2B3F58] border border-[#2B3F58] md:rounded-[30px] rounded-[10px] md:px-[26px] px-[16px] py-[12px] flex gap-[5px] w-full md:w-auto">
+      <p className="flex gap-[5px] border-[#2B3F58] px-[16px] md:px-[26px] py-[12px] border rounded-[10px] md:rounded-[30px] w-full md:w-auto text-[#2B3F58]">
         <CiSearch className="text-[#2B3F58]" />
         <b>Search</b>
-        <span className="hidden md:inline">
+        <span className="md:inline hidden">
           for an apartment, packages, or add-one
         </span>
       </p>
